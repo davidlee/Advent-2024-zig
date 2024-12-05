ce932d30b45066cb967350c826ddcadcc44bfa7e
David Lee <dav@davlee.com>
2024-12-06 00:01:29 +1100

    day 5 pt 2 , ugly as

--- src/day05.zig
+++ src/day05.zig
@@ -1,46 +1,215 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-
-const util = @import("util.zig");
-const gpa = util.gpa;
-
-const data = @embedFile("data/day05.txt");
-
-pub fn main() !void {
-    
-}
-
-// Useful stdlib functions
-const tokenizeAny = std.mem.tokenizeAny;
-const tokenizeSeq = std.mem.tokenizeSequence;
-const tokenizeSca = std.mem.tokenizeScalar;
-const splitAny = std.mem.splitAny;
-const splitSeq = std.mem.splitSequence;
 const splitSca = std.mem.splitScalar;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
-
 const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
-
 const print = std.debug.print;
 const assert = std.debug.assert;
 
-const sort = std.sort.block;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
+var data: []const u8 = @embedFile("data/day05.txt");
+var sample: []const u8 = @embedFile("data/day05_sample.txt");
 
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
+// const Map = std.AutoHashMap;
+// const StrMap = std.StringHashMap;
+// const BitSet = std.DynamicBitSet;
+// const BArray = std.BoundedArray;
+
+// Useful stdlib functions
+// const tokenizeAny = std.mem.tokenizeAny;
+// const tokenizeSeq = std.mem.tokenizeSequence;
+// const tokenizeSca = std.mem.tokenizeScalar;
+// const splitAny = std.mem.splitAny;
+// const splitSeq = std.mem.splitSequence;
+const indexOf = std.mem.indexOfScalar;
+// const indexOfAny = std.mem.indexOfAny;
+// const indexOfStr = std.mem.indexOfPosLinear;
+
+const lastIndexOf = std.mem.lastIndexOfScalar;
+// const lastIndexOfAny = std.mem.lastIndexOfAny;
+// const lastIndexOfStr = std.mem.lastIndexOfLinear;
+// const trim = std.mem.trim;
+// const sliceMin = std.mem.min;
+// const sliceMax = std.mem.max;
+// const eql = std.mem.eql;
+
+// const parseFloat = std.fmt.parseFloat;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    var m = alloc.create(Validator) catch unreachable;
+    m.init(alloc, &data);
+    defer m.deinit();
+
+    // m.sort();
+    // m.countValid();
+    m.countFixed();
+}
+
+const Rule = struct {
+    before: i32,
+    after: i32,
+};
+
+const Section = enum {
+    Rules,
+    Updates,
+};
+
+const Update = List(i32);
+const Reader = struct {
+    alloc: Allocator,
+    buffer: *[]const u8,
+    rules: List(Rule) = undefined,
+    updates: List(Update) = undefined,
+    section: Section = .Rules,
+
+    pub fn init(self: *Reader, alloc: Allocator, buffer: *[]const u8) void {
+        self.buffer = buffer;
+        self.alloc = alloc;
+        self.rules = List(Rule).init(alloc);
+        self.updates = List(Update).init(alloc);
+    }
+
+    pub fn deinit(self: *Reader) void {
+        // First deinit all the Update ArrayLists stored in updates
+        for (self.updates.items) |update| {
+            update.deinit();
+        }
+        self.updates.deinit();
+        self.rules.deinit();
+        // self.alloc.destroy(self);
+    }
+
+    pub fn load(self: *Reader) void {
+        var lines = splitSca(u8, (self.buffer.*), '\n');
+        var it = lines.next();
+        while (it) |line| : (it = lines.next()) {
+            switch (self.section) {
+                .Rules => {
+                    if (line.len == 0) {
+                        self.section = .Updates;
+                        continue;
+                    }
+                    var bits = splitSca(u8, line, '|');
+                    const rule = Rule{
+                        .before = parseInt(i32, bits.next().?, 10) catch unreachable,
+                        .after = parseInt(i32, bits.next().?, 10) catch unreachable,
+                    };
+                    self.rules.append(rule) catch unreachable;
+                },
+                .Updates => {
+                    var update: Update = Update.init(self.alloc);
+                    var bits = splitSca(u8, line, ',');
+                    var iter = bits.next();
+                    while (iter) |num| : (iter = bits.next()) {
+                        const n = parseInt(i32, num, 10) catch unreachable;
+                        update.append(n) catch unreachable;
+                    }
+                    self.updates.append(update) catch unreachable;
+                },
+            }
+        }
+    }
+};
+
+const Validator = struct {
+    reader: *Reader = undefined,
+    alloc: Allocator,
+
+    pub fn init(self: *Validator, alloc: Allocator, buffer: *[]const u8) void {
+        self.alloc = alloc;
+        self.reader = alloc.create(Reader) catch unreachable;
+        self.reader.init(alloc, buffer);
+        self.reader.load();
+    }
+
+    pub fn deinit(self: *Validator) void {
+        self.reader.deinit();
+        self.alloc.destroy(self.reader);
+        self.alloc.destroy(self);
+    }
+
+    pub fn check(self: *Validator, update: *const Update) bool {
+        for (self.reader.rules.items) |rule| {
+            if (!Validator.isSatisfied(update, &rule)) {
+                for (update.items) |item| {
+                    print("{d} ", .{item});
+                }
+                print("\n", .{});
+                print("Failed to satisfy rule {d} | {d}\n", .{
+                    rule.before,
+                    rule.after,
+                });
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /// Fix the update by swapping the before and after values of each rule not satisfied
+    pub fn fix(self: *Validator, update: *Update) void {
+        for (self.reader.rules.items) |rule| {
+            if (!Validator.isSatisfied(update, &rule)) {
+                const after_i = (indexOf(i32, update.items, rule.after)).?;
+                const before_i = (lastIndexOf(i32, update.items, rule.before)).?;
+                const before = update.orderedRemove(before_i);
+                update.insert(after_i, before) catch unreachable;
+                print("fixed {d}\n", .{before});
+                for (update.items) |item| {
+                    print("{d} ", .{item});
+                }
+                print("\n", .{});
+                return;
+            }
+        }
+        assert(self.check(update));
+    }
+
+    fn isSatisfied(update: *const Update, rule: *const Rule) bool {
+        const before = rule.before;
+        const after = rule.after;
+        const after_i = if (indexOf(i32, update.items, after)) |i| i else return true;
+        const before_i = if (lastIndexOf(i32, update.items, before)) |i| i else return true;
+        return before_i < after_i;
+    }
+
+    pub fn countValid(self: *Validator) void {
+        var count: i32 = 0;
+        for (self.reader.updates.items) |update| {
+            if (self.check(&update)) {
+                const i = @divFloor(update.items.len, 2);
+                count += update.items[i];
+            }
+        }
+        print("{d}\n", .{count});
+    }
+
+    pub fn countFixed(self: *Validator) void {
+        var count: i32 = 0;
+        for (self.reader.updates.items) |*update| {
+            if (self.check(update)) continue;
+            while (!self.check(update)) {
+                print("fixing,\n", .{});
+                for (update.items) |item| {
+                    print("{d} ", .{item});
+                }
+                print("\n", .{});
+                self.fix(update);
+            }
+            if (self.check(update)) {
+                const i = @divFloor(update.items.len, 2);
+                count += update.items[i];
+            } else {
+                print("Failed to fix update\n", .{});
+                for (update.items) |item| {
+                    print("{d} ", .{item});
+                }
+                print("\n", .{});
+            }
+        }
+        print("{d}\n", .{count});
+    }
+};
